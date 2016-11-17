26cf96959a7bbf8793a260ce48693da7fdb37831
刘文政 <wenzheng.liu@corp.elong.com>
2016-11-17 20:31:12 +0800

    登录页面

---
+++ app/src/main/java/com/oriental/coach/activity/LoginActivity.java
@@ -0,0 +1,46 @@
+package com.oriental.coach.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.oriental.coach.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * @Author lwz
+ * @Date 2016/11/15 0012
+ * @Describe 日程安排
+ */
+
+public class LoginActivity extends Activity {
+
+
+    @Bind(R.id.et_accounts)
+    EditText etAccounts;
+    @Bind(R.id.et_password)
+    EditText etPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+    }
+
+
+    @OnClick({R.id.btn_login})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+    }
+}
